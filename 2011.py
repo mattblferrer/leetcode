0d39c454a87cec02ebeb796db2eca26d3e4afdcb
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-08-17 22:31:40 +0800

    solved 2011

---
+++ 2011.py
@@ -0,0 +1,10 @@
+class Solution:
+    def finalValueAfterOperations(self, operations: list[str]) -> int:
+        x = 0
+        for operation in operations:
+            if operation in ["X++", "++X"]:
+                x += 1
+            else:
+                x -= 1
+
+        return x
