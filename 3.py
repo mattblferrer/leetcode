34ef6de695a0850c3f0195e105ac846463f26a8b
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-08-17 15:24:16 +0800

    optimized 3

--- 3.py
+++ 3.py
@@ -1,27 +1,28 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        seen = set()
+        seen = dict()  # stores index of char in substring
         maximum_length = 0
         current_length = 0
+        string_length = len(s)
         start_ptr = 0  # starting pointer index of s
         curr_ptr = 0  # current pointer index of s
 
         # loop through substrings until the end of s
-        while curr_ptr != len(s):
-            # if repeated char found, reset pointers and seen set
-            if s[curr_ptr] in seen:
-                current_length = 0
-                seen = set()
-                start_ptr += 1
-                curr_ptr = start_ptr
+        while curr_ptr < string_length:
+            char = s[curr_ptr]
 
-            # if repeated char not yet found
-            else:
-                seen.add(s[curr_ptr])
-                current_length += 1  
-                curr_ptr += 1
+            # if repeated char found, slide start_ptr to the right
+            if char in seen and seen[char] >= start_ptr:
+                current_length -= seen[char] - start_ptr + 1
+                start_ptr = seen[char] + 1
 
-                if maximum_length < current_length:  # update length
-                    maximum_length = current_length
+            # slide curr_ptr to the right
+            seen[char] = curr_ptr
+            curr_ptr += 1
+            current_length += 1
+
+            if maximum_length < current_length:  # update length
+                maximum_length = current_length
                 
         return maximum_length
+    
