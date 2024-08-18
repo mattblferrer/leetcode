ef025951774f2409f164e81b9f5ea4666325eb2f
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-08-18 12:16:59 +0800

    optimized 1323

--- 1323.py
+++ 1323.py
@@ -1,21 +1,4 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
-        digits = [int(digit) for digit in str(num)]
-        six_index = -1
-
-        # scan for leftmost 6 to be changed to 9
-        for i, digit in enumerate(digits):
-            if digit == 6: 
-                six_index = len(digits) - i - 1  # since reversed
-                break
-
-        # change 6 to 9 and form new number
-        maximum = 0
-        for i, digit in enumerate(reversed(digits)):
-            if i == six_index:
-                maximum += 10 ** i * 9
-            else: 
-                maximum += 10 ** i * digit
-
-        return maximum
+        return int(str(num).replace("6", "9", 1))
     
