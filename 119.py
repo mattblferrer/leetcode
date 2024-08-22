2e182cea330ca55ea3f51fe4ca0f5eea327ecd3d
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-08-22 17:39:46 +0800

    optimized 119

--- 119.py
+++ 119.py
@@ -1,12 +1,6 @@
+from math import factorial
+
 class Solution:
     def getRow(self, rowIndex: int) -> list[int]:
-        row = [1]  # first row
-
-        for _ in range(rowIndex):
-            next_row = [1]  # start row with 1
-            for a, b in zip(row, row[1:]):  # add two numbers directly above
-                next_row.append(a + b)
-            next_row.append(1)  # end row with 1
-            row = next_row
-
-        return row
+        return [int(factorial(rowIndex)/(factorial(k) * factorial(rowIndex-k))) 
+            for k in range(rowIndex + 1)]
