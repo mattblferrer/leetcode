ab426bfc79c081937cfdac88f656bd490d4ef070
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-08-27 22:01:18 +0800

    optimized 1551

--- 1551.py
+++ 1551.py
@@ -1,6 +1,4 @@
 class Solution:
     def minOperations(self, n: int) -> int:
         # get difference of arr[i] from median and add (simplified formula):
-        if n % 2 == 0:  # even
-            return n * n // 4
-        return (n // 2) * (n // 2 + 1)  # odd
+        return n * n // 4
