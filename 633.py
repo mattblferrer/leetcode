99a18a3fcb8f5fa787b4f858fc78976d40559a68
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-09-02 20:58:32 +0800

    optimized 633

--- 633.py
+++ 633.py
@@ -1,11 +1,8 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        for a in range(int((c // 2) ** 0.5) + 1):
+        for a in range(int((c // 2) ** 0.5) + 1):  # if even, search a, b pairs
             b = int((c - a * a) ** 0.5)  # calculate floor of sqrt(b)
-            sum_squares = a * a + b * b  # equal if sqrt(b) = floor(sqrt(b))
-            if sum_squares == c:
+            if a * a + b * b == c:  # equal if sqrt(b) = floor(sqrt(b))
                 return True
-            if sum_squares > c:
-                continue
 
         return False
