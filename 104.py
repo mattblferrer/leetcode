e7cbe1b74c909290e7b75442f7e7ec3cb6fa858c
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-09-04 19:56:18 +0800

    solved 104

---
+++ 104.py
@@ -0,0 +1,15 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int: # type: ignore
+        if not root.left and not root.right:  # reached leaf node
+            return 1
+        if not root.left: 
+            return 1 + self.maxDepth(root.right)
+        if not root.right:
+            return 1 + self.maxDepth(root.left)
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
