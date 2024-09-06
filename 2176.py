b378f5752f48a8e05708eb223ae215b6b5c03965
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-09-06 12:13:07 +0800

    solved 2176

---
+++ 2176.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countPairs(self, nums: list[int], k: int) -> int:
+        pairs = 0
+        for i in range(len(nums) - 1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] == nums[j] and (i * j) % k == 0:
+                    pairs += 1
+        
+        return pairs
