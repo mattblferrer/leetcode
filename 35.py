439e5a073f2881c368f11f98d995a1f931478820
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-09-07 01:56:40 +0800

    optimized 35

--- 35.py
+++ 35.py
@@ -6,13 +6,13 @@ class Solution:
         if nums[left] >= target:  # if less than all elements
             return 0
 
-        while left != right and left != right - 1:  # binary search
+        while left <= right:  # binary search
             guess = (left + right) // 2
             if nums[guess] == target:
                 return guess
             elif nums[guess] > target:  # target to the left of guess
-                right = guess
+                right = guess - 1
             elif nums[guess] < target:  # target to the right of guess
-                left = guess
+                left = guess + 1
 
-        return right  # return insert position if element not found
+        return left  # return insert position if element not found
