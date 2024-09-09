797c65269d03e6115d8363a676a8f599c0c5a144
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-09-09 23:24:10 +0800

    solved 1873

---
+++ 1873.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def calculate_special_bonus(employees: pd.DataFrame) -> pd.DataFrame:
+    employees['bonus'] = employees['salary']
+    employees.loc[employees['name'].str[0] == 'M', 'bonus'] = 0
+    employees.loc[employees['employee_id'] % 2 == 0, 'bonus'] = 0
+    return employees[['employee_id', 'bonus']].sort_values(by='employee_id')
