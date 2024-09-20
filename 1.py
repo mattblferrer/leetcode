6a04f4d52cde06f1ad963f8c578696999cfdff48
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-09-20 13:00:06 +0800

    improved 1

--- 1.py
+++ 1.py
@@ -1,13 +1,10 @@
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
-        nums_dict = dict((v, k) for k, v in enumerate(nums))
+        nums_dict = dict()
 
         for i, x in enumerate(nums):
             y = target - x
-            try:
-                j = nums_dict[y]
-            except KeyError:
-                continue
-            if i != j:
-                return [i, j]
+            if y in nums_dict:
+                return [i, nums_dict[y]]
+            nums_dict[x] = i
             
