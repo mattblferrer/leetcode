7106033a6129821953b774c3e2001484bdac66a9
Matt Ferrer <63775830+mattblferrer@users.noreply.github.com>
2024-09-25 00:04:04 +0800

    optimized 1492

--- 1492.py
+++ 1492.py
@@ -1,10 +1,16 @@
 class Solution:
     def kthFactor(self, n: int, k: int) -> int:
-        factors = 0
-        for i in range(1, n + 1):
-            if n % i == 0:
-                factors += 1
-            if factors == k:
-                return i
+        factors1 = []  # factors below sqrt(n)
+        factors2 = []  # factors above sqrt(n)
 
-        return -1
+        for i in range(1, int(n ** 0.5) + 1):
+            if n % i == 0:  # since i * (n // i) = n
+                factors1.append(i)
+                if i != n // i:  # avoid adding same factor twice
+                    factors2.append(n // i)
+
+        factors2 = factors2[::-1]  # since factors2 in descending order
+        factors = factors1 + factors2
+        if len(factors) < k:
+            return -1
+        return factors[k - 1]  # get kth factor (k - 1 since 0-indexed)
